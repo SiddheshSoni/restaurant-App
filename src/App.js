fc226567f184e8cb60a5413ad1b7feaa278295c7
SiddheshSoni <work.siddheshsoni@gmail.com>
2025-08-10 16:54:56 +0530

    Added Cart comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 
 function App() {
   return (
     <div className="App">
+      
+      <Cart/>
       <Header />
       <Meals />
     </div>
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import classes from './Modal.module.css'
+import ReactDOM from "react-dom"
+const Backdrop =()=>{
+    return <div className={classes.backdrop} />
+}
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+        <div className={classes.content}>{props.children}</div>
+    </div>
+  )
+}
+const portalElement = document.getElementById("overlays")
+const Modal =(props)=>{
+    return(
+        <>
+            {ReactDOM.createPotal(<Backdrop/>,portalElement)}
+            {ReactDOM.createPotal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
+        </>
+    )
+}
+export default Modal
