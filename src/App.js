798960114ceefd6d8a3f9d71e5fb5ee83bac52da
SiddheshSoni <work.siddheshsoni@gmail.com>
2025-08-10 16:02:09 +0530

    added summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary"
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MealsSummary />
     </div>
   );
 }
